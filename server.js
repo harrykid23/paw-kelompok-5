7fe03b25cc55557ec743929c995773de0389ebdb
dickymuhr <dickymuhrizal@gmail.com>
2021-09-27 08:08:22 +0700

    adding entry poin

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.json({limit: '50mb'}));
 // parse requests of content-type: application/x-www-form-urlencoded (true) or 
 // content-type: application/form-data (false)
 app.use(bodyParser.urlencoded({extended: false, limit: '50mb', parameterLimit: 1000000}));
-//IGNORE - tes commit from vscode 1
 // Connect to Mongodb
 mongoose.connect("mongodb://localhost:27017/kuliah_webdev")
     .then(()=>{
@@ -21,6 +20,14 @@ mongoose.connect("mongodb://localhost:27017/kuliah_webdev")
         console.log(err.message || "Terjadi kesalahan saat menyambungkan Mongodb");
     })
 
-app.listen(3000, ()=>{
-    console.log("Server berjalan di http://localhost:3000/");
+const PORT = 3000
+app.listen(PORT, ()=>{
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
+//Entry poin and first route domain
+app.get('/',(req,res)=>{
+    res.json({
+        message:"Selamat datang! Perkenalkan kami kelompok 5"
+    })
 });
